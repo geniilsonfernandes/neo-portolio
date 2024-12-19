576cb97193413637bce76f45cffdad857e2eec3d
gedev <geniilsonfernandes@gmail.com>
2024-12-19 18:46:40 -0300

    add env

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,9 +1,9 @@
 import { Client } from "@notionhq/client";
 
-const databaseId = "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";
+
 // Initializing a client
 const notion = new Client({
-  auth: "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",
+  auth: process.env.NOTION_TOKEN,
 });
 
 export { notion };
